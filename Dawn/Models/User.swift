baa71b476dbf33aae13382589d35c162c739bc02
Sam Calvert <sam.clvrt@gmail.com>
2014-12-27 16:33:19 -0800

    add user class

---
+++ Dawn/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Dawn
+//
+//  Created by Sam Calvert on 12/27/14.
+//  Copyright (c) 2014 Elos. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var id: String?
+    var createdAt: NSDate?
+    var name: String?
+    var key: String?
+}
