b159a0abea80231a6c06ce7b5721c706e24fe2cb
Sam Calvert <sam.clvrt@gmail.com>
2015-01-10 18:24:45 -0800

    add Message model

---
+++ Dawn/Models/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Dawn
+//
+//  Created by Sam Calvert on 1/10/15.
+//  Copyright (c) 2015 Elos. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case Get
+    case Post
+    case Delete
+    case Error
+}
+
+class Message {
+    var type: Type!
+}
